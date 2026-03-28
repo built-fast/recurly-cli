e954f1bd1fb4bc18a3766bcdc59c111121e99af0
Josh Priddle <jpriddle@me.com>
2026-03-28 16:41:17 -0400

    Get Subscription Command

--- cmd/subscriptions.go
+++ cmd/subscriptions.go
@@ -18,6 +18,151 @@ func newSubscriptionsCmd() *cobra.Command {
 		Short: "Manage subscriptions",
 	}
 	cmd.AddCommand(newSubscriptionsListCmd())
+	cmd.AddCommand(newSubscriptionsGetCmd())
+	return cmd
+}
+
+func subscriptionDetailColumns() []output.Column {
+	return []output.Column{
+		{Header: "ID", Extract: func(v any) string { return v.(*recurly.Subscription).Id }},
+		{Header: "UUID", Extract: func(v any) string { return v.(*recurly.Subscription).Uuid }},
+		{Header: "Account Code", Extract: func(v any) string { return v.(*recurly.Subscription).Account.Code }},
+		{Header: "Plan Code", Extract: func(v any) string { return v.(*recurly.Subscription).Plan.Code }},
+		{Header: "Plan Name", Extract: func(v any) string { return v.(*recurly.Subscription).Plan.Name }},
+		{Header: "State", Extract: func(v any) string { return v.(*recurly.Subscription).State }},
+		{Header: "Currency", Extract: func(v any) string { return v.(*recurly.Subscription).Currency }},
+		{Header: "Unit Amount", Extract: func(v any) string {
+			return fmt.Sprintf("%.2f", v.(*recurly.Subscription).UnitAmount)
+		}},
+		{Header: "Quantity", Extract: func(v any) string {
+			return fmt.Sprintf("%d", v.(*recurly.Subscription).Quantity)
+		}},
+		{Header: "Subtotal", Extract: func(v any) string {
+			return fmt.Sprintf("%.2f", v.(*recurly.Subscription).Subtotal)
+		}},
+		{Header: "Tax", Extract: func(v any) string {
+			return fmt.Sprintf("%.2f", v.(*recurly.Subscription).Tax)
+		}},
+		{Header: "Total", Extract: func(v any) string {
+			return fmt.Sprintf("%.2f", v.(*recurly.Subscription).Total)
+		}},
+		{Header: "Collection Method", Extract: func(v any) string { return v.(*recurly.Subscription).CollectionMethod }},
+		{Header: "Auto Renew", Extract: func(v any) string {
+			return fmt.Sprintf("%t", v.(*recurly.Subscription).AutoRenew)
+		}},
+		{Header: "Current Period Started At", Extract: func(v any) string {
+			s := v.(*recurly.Subscription)
+			if s.CurrentPeriodStartedAt != nil {
+				return s.CurrentPeriodStartedAt.Format(time.RFC3339)
+			}
+			return ""
+		}},
+		{Header: "Current Period Ends At", Extract: func(v any) string {
+			s := v.(*recurly.Subscription)
+			if s.CurrentPeriodEndsAt != nil {
+				return s.CurrentPeriodEndsAt.Format(time.RFC3339)
+			}
+			return ""
+		}},
+		{Header: "Trial Started At", Extract: func(v any) string {
+			s := v.(*recurly.Subscription)
+			if s.TrialStartedAt != nil {
+				return s.TrialStartedAt.Format(time.RFC3339)
+			}
+			return ""
+		}},
+		{Header: "Trial Ends At", Extract: func(v any) string {
+			s := v.(*recurly.Subscription)
+			if s.TrialEndsAt != nil {
+				return s.TrialEndsAt.Format(time.RFC3339)
+			}
+			return ""
+		}},
+		{Header: "Paused At", Extract: func(v any) string {
+			s := v.(*recurly.Subscription)
+			if s.PausedAt != nil {
+				return s.PausedAt.Format(time.RFC3339)
+			}
+			return ""
+		}},
+		{Header: "Remaining Pause Cycles", Extract: func(v any) string {
+			return fmt.Sprintf("%d", v.(*recurly.Subscription).RemainingPauseCycles)
+		}},
+		{Header: "Net Terms", Extract: func(v any) string {
+			return fmt.Sprintf("%d", v.(*recurly.Subscription).NetTerms)
+		}},
+		{Header: "Net Terms Type", Extract: func(v any) string { return v.(*recurly.Subscription).NetTermsType }},
+		{Header: "PO Number", Extract: func(v any) string { return v.(*recurly.Subscription).PoNumber }},
+		{Header: "Gateway Code", Extract: func(v any) string { return v.(*recurly.Subscription).GatewayCode }},
+		{Header: "Billing Info ID", Extract: func(v any) string { return v.(*recurly.Subscription).BillingInfoId }},
+		{Header: "Created At", Extract: func(v any) string {
+			s := v.(*recurly.Subscription)
+			if s.CreatedAt != nil {
+				return s.CreatedAt.Format(time.RFC3339)
+			}
+			return ""
+		}},
+		{Header: "Updated At", Extract: func(v any) string {
+			s := v.(*recurly.Subscription)
+			if s.UpdatedAt != nil {
+				return s.UpdatedAt.Format(time.RFC3339)
+			}
+			return ""
+		}},
+		{Header: "Activated At", Extract: func(v any) string {
+			s := v.(*recurly.Subscription)
+			if s.ActivatedAt != nil {
+				return s.ActivatedAt.Format(time.RFC3339)
+			}
+			return ""
+		}},
+		{Header: "Canceled At", Extract: func(v any) string {
+			s := v.(*recurly.Subscription)
+			if s.CanceledAt != nil {
+				return s.CanceledAt.Format(time.RFC3339)
+			}
+			return ""
+		}},
+		{Header: "Expires At", Extract: func(v any) string {
+			s := v.(*recurly.Subscription)
+			if s.ExpiresAt != nil {
+				return s.ExpiresAt.Format(time.RFC3339)
+			}
+			return ""
+		}},
+	}
+}
+
+func newSubscriptionsGetCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "get <subscription_id>",
+		Short: "Get subscription details",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			c, err := newSubscriptionAPI()
+			if err != nil {
+				return err
+			}
+
+			format := viper.GetString("output")
+
+			subscription, err := c.GetSubscription(args[0])
+			if err != nil {
+				return err
+			}
+
+			columns := subscriptionDetailColumns()
+
+			formatted, err := output.FormatOne(format, columns, subscription)
+			if err != nil {
+				return err
+			}
+
+			_, err = fmt.Fprintln(cmd.OutOrStdout(), formatted)
+			return err
+		},
+	}
+
 	return cmd
 }
 
