2cfb631e92e1867c8323453c5e0a3ac20f526fe1
Josh Priddle <jpriddle@me.com>
2026-03-29 16:48:58 -0400

    Go Embed for SKILL.md

--- skills/embed.go
+++ skills/embed.go
@@ -2,10 +2,10 @@ package skills
 
 import "embed"
 
-//go:embed recurly/SKILL.md
-var FS embed.FS
+//go:embed recurly
+var Content embed.FS
 
 // SkillMD returns the raw contents of the embedded SKILL.md file.
 func SkillMD() ([]byte, error) {
-	return FS.ReadFile("recurly/SKILL.md")
+	return Content.ReadFile("recurly/SKILL.md")
 }
