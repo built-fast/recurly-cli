13878cc89ac574959f7186e49edfe33c0d49ef88
Josh Priddle <jpriddle@me.com>
2026-03-28 21:09:48 -0400

    CouponAPI Interface and Client Wiring

---
+++ cmd/coupon_api.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"github.com/built-fast/recurly-cli/internal/client"
+	recurly "github.com/recurly/recurly-client-go/v5"
+)
+
+// CouponAPI abstracts the Recurly SDK methods used by coupon commands,
+// allowing tests to inject mocks without making real API calls.
+type CouponAPI interface {
+	ListCoupons(params *recurly.ListCouponsParams, opts ...recurly.Option) (recurly.CouponLister, error)
+	GetCoupon(couponId string, opts ...recurly.Option) (*recurly.Coupon, error)
+	CreateCoupon(body *recurly.CouponCreate, opts ...recurly.Option) (*recurly.Coupon, error)
+	UpdateCoupon(couponId string, body *recurly.CouponUpdate, opts ...recurly.Option) (*recurly.Coupon, error)
+	DeactivateCoupon(couponId string, opts ...recurly.Option) (*recurly.Coupon, error)
+	RestoreCoupon(couponId string, body *recurly.CouponUpdate, opts ...recurly.Option) (*recurly.Coupon, error)
+	GenerateUniqueCouponCodes(couponId string, body *recurly.CouponBulkCreate, opts ...recurly.Option) (*recurly.UniqueCouponCodeParams, error)
+	ListUniqueCouponCodes(couponId string, params *recurly.ListUniqueCouponCodesParams, opts ...recurly.Option) (recurly.UniqueCouponCodeLister, error)
+}
+
+// newCouponAPI is the factory function used by coupon commands to get an API client.
+// Tests override this to inject mocks.
+var newCouponAPI = func() (CouponAPI, error) {
+	return client.NewClient()
+}
