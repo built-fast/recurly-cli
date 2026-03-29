002cc47a41ccf61aad4aa3ae7a2eb1dc618eeeb5
Josh Priddle <jpriddle@me.com>
2026-03-28 21:33:22 -0400

    Account Coupon Redemptions API Interface

---
+++ cmd/account_redemption_api.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"github.com/built-fast/recurly-cli/internal/client"
+	recurly "github.com/recurly/recurly-client-go/v5"
+)
+
+// AccountRedemptionAPI abstracts the Recurly SDK methods used by account coupon redemption commands,
+// allowing tests to inject mocks without making real API calls.
+type AccountRedemptionAPI interface {
+	ListAccountCouponRedemptions(accountId string, params *recurly.ListAccountCouponRedemptionsParams, opts ...recurly.Option) (recurly.CouponRedemptionLister, error)
+	ListActiveCouponRedemptions(accountId string, opts ...recurly.Option) (recurly.CouponRedemptionLister, error)
+	CreateCouponRedemption(accountId string, body *recurly.CouponRedemptionCreate, opts ...recurly.Option) (*recurly.CouponRedemption, error)
+	RemoveCouponRedemption(accountId string, opts ...recurly.Option) (*recurly.CouponRedemption, error)
+	GetCouponRedemption(accountId string, couponRedemptionId string, opts ...recurly.Option) (*recurly.CouponRedemption, error)
+}
+
+// newAccountRedemptionAPI is the factory function used by account coupon redemption commands to get an API client.
+// Tests override this to inject mocks.
+var newAccountRedemptionAPI = func() (AccountRedemptionAPI, error) {
+	return client.NewClient()
+}
---
+++ cmd/account_redemption_api_test.go
@@ -0,0 +1,57 @@
+package cmd
+
+import (
+	recurly "github.com/recurly/recurly-client-go/v5"
+)
+
+// mockAccountRedemptionAPI implements AccountRedemptionAPI for testing.
+type mockAccountRedemptionAPI struct {
+	listAccountCouponRedemptionsFn func(accountId string, params *recurly.ListAccountCouponRedemptionsParams, opts ...recurly.Option) (recurly.CouponRedemptionLister, error)
+	listActiveCouponRedemptionsFn  func(accountId string, opts ...recurly.Option) (recurly.CouponRedemptionLister, error)
+	createCouponRedemptionFn       func(accountId string, body *recurly.CouponRedemptionCreate, opts ...recurly.Option) (*recurly.CouponRedemption, error)
+	removeCouponRedemptionFn       func(accountId string, opts ...recurly.Option) (*recurly.CouponRedemption, error)
+	getCouponRedemptionFn          func(accountId string, couponRedemptionId string, opts ...recurly.Option) (*recurly.CouponRedemption, error)
+}
+
+func (m *mockAccountRedemptionAPI) ListAccountCouponRedemptions(accountId string, params *recurly.ListAccountCouponRedemptionsParams, opts ...recurly.Option) (recurly.CouponRedemptionLister, error) {
+	if m.listAccountCouponRedemptionsFn != nil {
+		return m.listAccountCouponRedemptionsFn(accountId, params, opts...)
+	}
+	return nil, nil
+}
+
+func (m *mockAccountRedemptionAPI) ListActiveCouponRedemptions(accountId string, opts ...recurly.Option) (recurly.CouponRedemptionLister, error) {
+	if m.listActiveCouponRedemptionsFn != nil {
+		return m.listActiveCouponRedemptionsFn(accountId, opts...)
+	}
+	return nil, nil
+}
+
+func (m *mockAccountRedemptionAPI) CreateCouponRedemption(accountId string, body *recurly.CouponRedemptionCreate, opts ...recurly.Option) (*recurly.CouponRedemption, error) {
+	if m.createCouponRedemptionFn != nil {
+		return m.createCouponRedemptionFn(accountId, body, opts...)
+	}
+	return nil, nil
+}
+
+func (m *mockAccountRedemptionAPI) RemoveCouponRedemption(accountId string, opts ...recurly.Option) (*recurly.CouponRedemption, error) {
+	if m.removeCouponRedemptionFn != nil {
+		return m.removeCouponRedemptionFn(accountId, opts...)
+	}
+	return nil, nil
+}
+
+func (m *mockAccountRedemptionAPI) GetCouponRedemption(accountId string, couponRedemptionId string, opts ...recurly.Option) (*recurly.CouponRedemption, error) {
+	if m.getCouponRedemptionFn != nil {
+		return m.getCouponRedemptionFn(accountId, couponRedemptionId, opts...)
+	}
+	return nil, nil
+}
+
+func setMockAccountRedemptionAPI(mock *mockAccountRedemptionAPI) func() {
+	orig := newAccountRedemptionAPI
+	newAccountRedemptionAPI = func() (AccountRedemptionAPI, error) {
+		return mock, nil
+	}
+	return func() { newAccountRedemptionAPI = orig }
+}
